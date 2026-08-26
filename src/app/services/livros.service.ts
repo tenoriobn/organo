67dca1da31518e42e740b392013b054e736948b4
Bruno Tenorio <tenoriobn@gmail.com>
2026-08-26 09:37:43 -0300

    feat: :sparkles: Módulo 01: Listando dados com o método GET

---
+++ src/app/services/livros.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Livro } from '../componentes/livro/livro';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LivrosService {
+  private API_URL = 'http://localhost:3000/livros';
+
+  constructor(private httpClient: HttpClient) {}
+
+  obterLivros(): Observable<Livro[]> {
+    return this.httpClient.get<Livro[]>(this.API_URL);
+  }
+}
